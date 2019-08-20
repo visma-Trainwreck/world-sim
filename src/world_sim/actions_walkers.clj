b5321c294d0197e22f068aa1e3f892701702567f
Visma-Trainwreck <peter.l.rasmussen@visma.com>
2019-08-20 11:53:19 +0200

    small code change. branch rename

--- src/world_sim/actions_walkers.clj
+++ src/world_sim/actions_walkers.clj
@@ -7,12 +7,23 @@
   :events [;;is hungry, is sleepy, is hurt, is alone
   ])
 
+(defn enqueue
+  [entity goal]
+  (-> (get-in entity [:mind-map :current-goal])
+      (conj goal)
+      (as-> goals
+            (assoc-in entity [:mind-map :current-goal] goals))))
 
-
+(defn goals
+  [world entity-class entity]
+  (if (empty? (get-in entity [:mind-map :current-goal]))
+    (cond entity
+          (< (:hunger entity) 10) (enqueue entity :eat))))
 
 (def mind-map
-  {:current-goal (fn [world entity-class entity]
-                   (if))
-   :current-path []
-   :calc-goal (fn [world entity-class entity] true)})
+  {:current-goal [:travel :eat]
+   :current-path [[1 1] [2 2] [3 2]]
+   :calc-goal    (fn [world entity-class entity]
+                   (cond entity
+                         (< (:hunger entity) 10) (enqueue entity :eat)))})
 
